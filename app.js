1e753fe0e55124f4b3071458d38bf63bbb513f52
ae95caba <ae95caba@gmail.com>
2023-09-01 01:55:34 -0300

    fix missing flavours router in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require(`./routes/products`);
+var flavoursRouter = require(`./routes/flavours`);
 const cors = require("cors");
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+app.use("/flavours", flavoursRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
